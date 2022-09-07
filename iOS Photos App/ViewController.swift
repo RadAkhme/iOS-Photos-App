ffc78d8e2fd2fc377f56b679db6eb13a9b5cab30
RadikAkhme <a.radik.s@ya.ru>
2022-09-07 19:41:11 +0300

    Initial Commit

---
+++ iOS Photos App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Photos App
+//
+//  Created by Радик Ахметзянов on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
