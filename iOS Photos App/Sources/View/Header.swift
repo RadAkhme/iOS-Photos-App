183bbaff90922b026c973f677e1de6c0a40df4d3
RadikAkhme <a.radik.s@ya.ru>
2022-09-09 00:04:59 +0300

    Add header.

--- iOS Photos App/Sources/View/Header.swift
+++ iOS Photos App/Sources/View/Header.swift
@@ -8,5 +8,45 @@
 import UIKit
 
 class Header: UICollectionReusableView {
-        
+    
+    static let identifier = "Header"
+    
+    // MARK: - Outlets
+    
+    lazy var title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        return label
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Error in Cell")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        addSubview(title)
+    }
+    
+    private func setupLayout() {
+        title.snp.makeConstraints { make in
+            make.bottom.equalTo(self)
+            make.left.equalTo(self)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        title.text = nil
+    }
 }
